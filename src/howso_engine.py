129b3afd338f7d3782a72043e56c7a9bffecc624
Anonymity941212 <151071098+Anonymity941212@users.noreply.github.com>
2023-11-16 13:36:06 -0500

    Add files via upload

---
+++ src/howso_engine.py
@@ -0,0 +1,104 @@
+import random
+import time
+from pathlib import Path
+from typing import Literal, Tuple
+
+from howso.engine import Trainee
+from howso.utilities import infer_feature_attributes
+from pandas import DataFrame, Series
+from scipy.io import arff
+from sklearn.model_selection import train_test_split
+
+
+def howsoOversampling(
+    X_train: DataFrame,
+    y_train: Series,
+    *,
+    desired_conviction: float = 5,
+    generate_new_cases: Literal["always", "attempt", "no"] = "no",
+    use_regional_model_residuals: bool = False,
+) -> Tuple[float, DataFrame, Series]:
+    """
+    Synthesize a balanced verison of an imbalanced dataset using Howso Engine.
+
+    Parameters
+    ----------
+    X_train : DataFrame
+        The predictors to train on.
+    y_train : Series
+        The predictand to train on.
+    desired_conviction : float, default 5
+        The desired conviction to use when synthesizing the dataset.
+    generate_new_cases : {"always", "attempt", "no"}, default "no"
+        Whether to enforce privacy constraints when generating.
+    use_regional_model_residuals : bool, default False
+        Whether to use regional model residuals when generating.
+    """
+    start_time = time.time()
+    target_feature = y_train.name
+    action_features = X_train.columns.values.tolist() + [target_feature]
+    X_train[target_feature] = y_train.values
+
+    features = infer_feature_attributes(X_train)
+    for attributes in features.values():
+        if attributes["type"] == "nominal":
+            attributes["non_sensitive"] = True
+
+    context_shape = int(X_train.shape[0] / 2)
+    contexts = [[1]] * context_shape + [[0]] * context_shape
+    context_features = [target_feature]
+
+    t = Trainee(
+        features=features,
+        overwrite_existing=True
+    )
+
+    print(X_train)
+
+    t.train(X_train)
+    t.analyze()
+
+    reaction = t.react(
+        action_features=action_features,
+        context_features=context_features,
+        contexts=contexts,
+        desired_conviction=desired_conviction,
+        generate_new_cases=generate_new_cases,
+        num_cases_to_generate=len(contexts),
+        use_regional_model_residuals=use_regional_model_residuals,
+    )
+
+    print(reaction["action"])
+    print(reaction["context"])
+    rt = time.time() - start_time
+    X_train_new = reaction['action'].iloc[:, :-1]
+    y_train_new = reaction['action'].iloc[:, -1]
+
+    return rt, X_train_new, y_train_new
+
+
+if __name__ == "__main__":
+    proj_path = Path(__file__).parent.parent.resolve()
+    data_path = proj_path / "data" / "Vulnerable_Files" / "moodle-2_0_0-metrics.arff"
+    data, _ = arff.loadarff(data_path)
+
+    df = DataFrame(data).astype({"IsVulnerable": str})
+    vulnerable_map = {'b\'yes\'': 1, 'b\'no\'': 0}
+    df["IsVulnerable"] = df["IsVulnerable"].map(vulnerable_map).fillna(df["IsVulnerable"])
+
+    df.drop_duplicates(inplace=True)
+    df.reset_index(inplace=True, drop=True)
+
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+
+    rs = random.randint(0, 100_000)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, stratify=y, random_state=rs
+    )
+    rt, X_train_new, y_train_new = howsoOversampling(X_train=X_train, y_train=y_train)
+
+    print(X_train_new)
+    print("")
+    print(y_train_new)
+    print("y train ratio: 1:" + str(round(y_train_new.value_counts()[0] / y_train_new.value_counts()[1])))
---
+++ src/utils.py
@@ -0,0 +1,107 @@
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score, accuracy_score, f1_score, roc_auc_score, precision_score
+
+import math
+import numpy as np
+import os
+from scipy.io import arff
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from lightgbm import LGBMClassifier
+
+def evaluate_result(y_pred, y_true):
+    tn, fp, fn, tp = confusion_matrix(y_true=y_true, y_pred=y_pred).ravel()
+
+    recall = recall_score(y_true=y_true, y_pred=y_pred)
+    fpr = fp / (fp + tn)
+    precision = precision_score(y_true=y_true, y_pred=y_pred, zero_division=0)
+    accuracy = accuracy_score(y_true=y_true, y_pred=y_pred)
+    f1 = f1_score(y_true=y_true, y_pred=y_pred)
+    roc_auc = roc_auc_score(y_true=y_true, y_score=y_pred)
+    g_score = 2 * recall * (1 - fpr) / (recall + 1 - fpr)
+    d2h = math.sqrt((1 - recall)**2 + (0 - fpr)**2) / math.sqrt(2)
+
+    return [round(accuracy, 3), 
+            round(precision, 3), 
+            round(recall, 3), 
+            round(fpr, 3), 
+            round(f1, 3), 
+            round(roc_auc, 3), 
+            round(g_score, 3), 
+            round(d2h, 3)]
+
+
+def parse_results(res):
+    res = res.replace('(', '').replace(')', '').replace('[', '').replace(']', '')
+    res = res.replace('array', '').replace(' ', '')
+    res_to_list = res.split(",")
+    res_to_list = [float(i) for i in res_to_list]
+    res_to_list[0] = int(np.round_(res_to_list[0]))
+    res_to_list[1] = int(np.round_(res_to_list[1]))
+    res_to_list[2] = int(np.round_(res_to_list[2]))
+
+    return res_to_list
+
+
+def read_data(project):
+    if project == "Moodle_Vuln":
+        data_path = f"{os.getcwd()}/data/Vulnerable_Files/moodle-2_0_0-metrics.arff"
+        data = arff.loadarff(data_path)
+        df = pd.DataFrame(data[0])
+        df['IsVulnerable'] = df['IsVulnerable'].astype('str')
+        d = {'b\'yes\'': 1, 'b\'no\'': 0}
+        df['IsVulnerable'] = df['IsVulnerable'].astype(str).map(d).fillna(df['IsVulnerable'])
+        print("before drop duplicates", df.shape[0])
+        df = df.drop_duplicates()
+        df.reset_index(inplace=True, drop=True)
+        print("after drop duplicates", df.shape[0])
+        return df
+    elif project in ["Defect_Eclipse_JDT_Core", "Defect_Eclipse_PDE_UI", "Defect_Mylyn"]:
+        fname = "_".join(project.split("_")[1:])
+        data_path = f"{os.getcwd()}/data/imbalance_defects_prediction/7_CK_NET_PROC/input/{fname}--CK_NET_PROC.arff"
+        data = arff.loadarff(data_path)
+        df = pd.DataFrame(data[0])
+        df['isBug'] = df['isBug'].astype('str')
+        d = {'b\'YES\'': 1, 'b\'NO\'': 0}
+        df['isBug'] = df['isBug'].astype(str).map(d).fillna(df['isBug'])
+        print("before drop duplicates", df.shape[0])
+        df = df.drop_duplicates()
+        df.reset_index(inplace=True, drop=True)
+        print("after drop duplicates", df.shape[0])
+        return df
+    elif project == "JS_Vuln":
+        data_path = f"{os.getcwd()}/data/JavaScript_Vulnerability/JSVulnerabilityDataSet-1.0.csv"
+        df = pd.read_csv(data_path)
+        drop_columns = ["name", "longname", "path", "full_repo_path", "line", "column", "endline", "endcolumn"]
+        df = df.drop(drop_columns, axis=1)
+        print("before drop duplicates", df.shape[0])
+        df = df.drop_duplicates()
+        df.reset_index(inplace=True, drop=True)
+        print("after drop duplicates", df.shape[0])
+        return df
+    elif project == "Ambari_Vuln":
+        train_data_path = f"{os.getcwd()}/data/Bug_Reports/ambari-train.csv"
+        test_data_path = f"{os.getcwd()}/data/Bug_Reports/ambari-test.csv"
+        train_df = pd.read_csv(train_data_path)
+        test_df = pd.read_csv(test_data_path)
+        return train_df, test_df
+
+
+def create_models():
+    clf_SVM = SVC()
+    clf_KNN = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
+    clf_LR = LogisticRegression(random_state=42, solver="saga", max_iter=20000, n_jobs=-1)
+    clf_DT = DecisionTreeClassifier()
+    clf_RF = RandomForestClassifier(random_state=42, n_jobs=-1)
+    clf_LightGBM = LGBMClassifier(objective="binary", random_state=42, n_jobs=-1)
+    clf_Adaboost = AdaBoostClassifier(n_estimators=100, random_state=42)
+    clf_GBDT = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, random_state=42)
+
+    return clf_SVM, clf_KNN, clf_LR, clf_DT, clf_RF, clf_LightGBM, clf_Adaboost, clf_GBDT
